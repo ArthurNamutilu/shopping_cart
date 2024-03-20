ffc7e21029541beee593638933be2f786458e392
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-03-20 23:44:47 +0300

    added cart page

--- src/components/product-tile/index.jsx
+++ src/components/product-tile/index.jsx
@@ -1,4 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, removeFromCart } from "../../store/slices/cart-slice";
+
 function ProductTile({ product }) {
+  const dispatch = useDispatch();
+  const { cart } = useSelector((state) => state);
+
+  function handleAddToCart() {
+    dispatch(addToCart(product));
+  }
+
+  function handleRemoveToCart() {
+    dispatch(removeFromCart(product.id));
+    console.log("removed");
+  }
+
   return (
     <div>
       <div className="group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
@@ -8,6 +23,25 @@ function ProductTile({ product }) {
             alt={product?.title}
             className="object-cover h-full w-full"
           />
+        </div>
+        <div>
+          <h1 className="w-40 truncate mt-3 text-gray-700 font-bold text-lg">
+            {product.title}
+          </h1>
+        </div>
+        <div className="flex items-center justify-center w-full mt-5">
+          <button
+            className="bg-red-950 text-white border-2 rounded-lg font-bold p-4"
+            onClick={
+              cart.some((item) => item.id === product.id)
+                ? handleRemoveToCart
+                : handleAddToCart
+            }
+          >
+            {cart.some((item) => item.id === product.id)
+              ? "Remove from cart"
+              : "Add to cart"}
+          </button>
         </div>
       </div>
     </div>
